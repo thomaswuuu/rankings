c5d4e5780cae5d6d386e4693423113dbb9f7e642
numb1086 <numb1086@users.noreply.github.com>
2023-11-10 11:37:28 +0800

    Add authentication check of autoupdate

--- routes/autoupdate.js
+++ routes/autoupdate.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const updateContorller = require("../controllers/updateController");
+const auth = require("./auth");
+const ach = auth.apiAuthCheck;
 
-router.get("/:command", updateContorller.autoUpdateTime);
+router.get("/:command", ach, updateContorller.autoUpdateTime);
 
 module.exports = router;
